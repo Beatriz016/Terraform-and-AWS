79e16daf8e07c2a214e900517b215e35a72fee43
Beatriz016 <beatrizvika@gmail.com>
2025-04-01 19:25:38 -0300

    Created Security Group

--- Project 1/main.tf
+++ Project 1/main.tf
@@ -63,3 +63,45 @@ resource "aws_route_table_association" "art" {
   subnet_id = aws_subnet.subnet-01.id
   route_table_id = aws_route_table.prod-route-table.id
 }
+
+# 6. Create Security Group to allow port 22, 80 and 443 
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id     = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1" # any prtocol
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
