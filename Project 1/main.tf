a655f9ac984b6cffeb10781b919112bf471a2159
Beatriz016 <beatrizvika@gmail.com>
2025-04-01 18:47:52 -0300

    added tag to vpc resource and created internet gateway resource

--- Project 1/main.tf
+++ Project 1/main.tf
@@ -15,6 +15,15 @@ provider "aws" {
 }
 
 
-resource "aws_vpc" "main" {
+# 1. Create VPC 
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# 2. Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
 }
