27dceef13691f2ffef18c419f219486048b48a23
Beatriz016 <beatrizvika@gmail.com>
2025-04-01 19:35:56 -0300

    Created Network Interface with an IP in the subnet

--- Project 1/main.tf
+++ Project 1/main.tf
@@ -105,3 +105,13 @@ resource "aws_security_group" "allow_web" {
     Name = "allow_web"
   }
 }
+
+# 7. Create a Network Interface with an IP address in the subnet that was created
+resource "aws_network_interface" "web-server-01" {
+  subnet_id = " aws_subnet.subnet-01.id"
+  private_ips = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+  }
+
+  
